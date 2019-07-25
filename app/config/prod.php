fd3ef0c9b4c7fbccfa85543596944a7b29726caf
fravii848 <51963024+fravii848@users.noreply.github.com>
2019-07-25 09:12:01 -0300

    Update prod.php

--- app/config/prod.php
+++ app/config/prod.php
@@ -7,6 +7,6 @@
 $app['telegram.settings'] = [
     'bot_key' => '902735034:AAF2mi1KzRKJBT1kn3WCi6my1mWVak2i5JQ',
     'bot_name' => 'FraviiuRobot',
-    'webhook' => 'https://fraviu92.herokuapp.com/action',
+    'webhook' => 'https://fraviu93.herokuapp.com/action',
     'command_path' => __DIR__.'/../../src/SatanGoss/Command/',
 ];
